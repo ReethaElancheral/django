f32a699ebec7425fe1ba1841e80bc36c69c8cbec
ReethaElancheral <reethapython@gmail.com>
2025-08-24 00:45:43 +0300

    Add files via upload

--- day78/newsletter/Newsletter_Project/settings.py
+++ day78/newsletter/Newsletter_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-))gpx1o908(^@xz4x%=@c&c4q%+^_(9xt+)*^67*ni5fng!9li
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
